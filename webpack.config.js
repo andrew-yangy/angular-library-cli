99e48641da0c0e673389841365b801d1cd58536d
Andrew <ddvkid@gmail.com>
2017-10-08 18:58:06 +1100

    serve: use webpack api instead of spawning webpack-dev-server

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { TsConfigPathsPlugin } = require('awesome-typescript-loader');
+const webpack = require('webpack');
+const CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
+
+module.exports = {
+    entry : {
+        'app': './src/demo/main.ts',
+        'polyfills': './src/demo/polyfills.ts',
+        'vendor': './src/demo/vendor.ts',
+    },
+    output: {
+        path: path.join(process.cwd(),'dist'),
+        publicPath: 'http://localhost:8080/',
+        filename: "js/[name].js"
+    },
+    resolve : {
+        extensions: ['.ts', '.js', '.json', '.css', '.scss', '.html'],
+        plugins: [
+            new TsConfigPathsPlugin({
+                configFileName: 'src/demo/tsconfig.json',
+                compiler: 'typescript'
+            })
+        ]
+    },
+    resolveLoader: {
+        modules: [path.join(__dirname, 'node_modules')]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loaders: ['awesome-typescript-loader?configFileName=./src/demo/tsconfig.json', 'angular2-template-loader'],
+                exclude: [/\.(spec|e2e)\.ts$/, /node_modules\/(?!(ng2-.+))/],
+            },
+
+            {
+                test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'file-loader?name=fonts/[name].[hash].[ext]?'
+            },
+
+            { test: /\.json$/, loader: 'json-loader' },
+            // },
+            {
+                test: /\.component\.css$/,
+                use: ['raw-loader']
+            },
+            {
+                test: /\.css$/,
+                exclude: /\.component\.css$/,
+                use: ['style-loader']
+            },
+            {test: /\.(scss|sass)$/, exclude: root('src', 'demo', 'style'), loader: 'raw-loader!postcss-loader!sass-loader'},
+
+            // support for .html as raw text
+            { test: /\.html$/, loader: 'raw-loader'}
+        ]
+    },
+    plugins: [
+        new webpack.ContextReplacementPlugin(
+            /angular(\\|\/)core(\\|\/)@angular/,
+            path.join(process.cwd(),'./src/demo/')
+        ),
+        new HtmlWebpackPlugin({
+            template: './src/demo/index.html',
+            chunksSortMode: 'dependency'
+        }),
+        new CommonsChunkPlugin({
+            names: ['vendor', 'polyfills']
+        }),
+    ]
+};
+function root(args) {
+    args = Array.prototype.slice.call(arguments, 0);
+    return path.join.apply(path, [__dirname].concat(args));
+}
